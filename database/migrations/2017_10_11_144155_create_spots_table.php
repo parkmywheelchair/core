1e2cc44ef68160f169d98b7bfdd63fb07bb1973c
Juan Pablo Flores Galindo <juanfg@outlook.com>
2017-10-11 17:01:48 +0200

    Added the migrations for the database

--- database/migrations/2017_10_11_144155_create_spots_table.php
+++ database/migrations/2017_10_11_144155_create_spots_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateSpotsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('spots', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->string('profile_picture');
-            $table->integer('points');
+            $table->double('latitude');
+            $table->double('length');
+            $table->integer('country_id')->unsigned()->nullable();
+            $table->foreign('country_id')->references('id')->on('countries')->onUpdate('cascade')->onDelete('set null');
+            $table->integer('rank');
             $table->boolean('visible');
             $table->timestamps();
         });
@@ -33,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('spots');
     }
 }
